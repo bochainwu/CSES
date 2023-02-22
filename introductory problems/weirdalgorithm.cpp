34e47268ce1a757ae45a0989bf5ad882bbb6378c
bochain <bochainwu@gmail.com>
2023-02-22 22:35:28 +0800

    weird algorithm

---
+++ introductory problems/weirdalgorithm.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    long long int a;
+    cin>>a;
+    while (a != 1){
+        cout<<a <<" ";
+        if(a%2 == 0){
+            a/=2;
+        }else{
+            a = a*3+1;
+        }
+    }
+    cout<<1;
+}
